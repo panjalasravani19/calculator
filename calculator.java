5babdbe5cda88e0c6eed03f6ae105d8adbc813af
panjalasravani19 <panjalasravani19@gmail.com>
2026-02-25 15:09:31 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,16 +1,13 @@
 class Calculator
 {
-public static void main(String args[])
-{
-int n1=Integer.parseInt(args[0]);
-int n2=Integer.parseInt(args[1]);
-int r1 = n1+n2;
-int r2 = n1-n2;
-int r3 = n1*n2;
-int r4 = n1/n2;
-System.out.println(“Addition: “+r1);
-System.out.println(“Subtraction: “+r2);
-System.out.println(“Multiplication: “+r3);
-System.out.println(“Division: “+r4);
-}
+  public static void main(String args[])
+  {
+    int n1 = Integer.parseInt(args[0]);
+    int n2 = Integer.parseInt(args[1]);
+    System.out.println("Addition is:"+(n1+n2));
+    System.out.println("Subtraction is:"+(n1-n2));
+    System.out.println("Multiplication is:"+(n1*n2));
+    System.out.println("Division is:"+(n1/n2));
+    System.out.println("Changes updated");
+  }
 }
